638b722807d0551201956521837170afe8131e2b
Bahar Işıklı <baharrisikli@gmail.com>
2026-07-20 18:49:02 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import authRoutes from "./routes/auth.routes.js";
+import helmet from "helmet";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors({
+  origin: "http://localhost:5173",
+}));
+app.use(helmet());
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+
+
+app.get("/", (req, res) => {
+  res.json({ message: "Atlas API çalışıyor " });
+});
+
+app.listen(PORT, () => {
+  console.log(`Sunucu http://localhost:${PORT} adresinde çalışıyor`);
+});
---
+++ src/routes/auth.routes.js
@@ -0,0 +1,126 @@
+import { Router } from "express";
+import bcrypt from "bcrypt";
+import prisma from "../lib/prisma.js";
+import jwt from "jsonwebtoken";
+import rateLimit from "express-rate-limit";
+import { z } from "zod";
+
+const router = Router();
+
+const registerSchema = z.object({
+  username: z.string().min(3).max(20),
+  email: z.string().email(),
+  password: z.string().min(6),
+});
+
+const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+});
+
+const loginLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 5,
+  message: {
+    message:
+      "Çok fazla giriş denemesi yaptınız, lütfen daha sonra tekrar deneyin.",
+  },
+});
+
+router.post("/register", async (req, res) => {
+  try {
+    const parsed = registerSchema.safeParse(req.body);
+    if (!parsed.success) {
+      const fieldErrors = parsed.error.issues.map((issue) => ({
+        field: issue.path[0],
+        message: issue.message,
+      }));
+      return res
+        .status(400)
+        .json({ message: "geçersiz veri ", errors: fieldErrors });
+    }
+    const { username, email, password } = parsed.data;
+
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        OR: [{ email }, { username }],
+      },
+    });
+
+    if (existingUser) {
+      return res
+        .status(409)
+        .json({ message: "Bu e-posta veya kullanıcı adı zaten kayıtlı." });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+      },
+    });
+    await prisma.board.create({
+      data: {
+        title: "Panom",
+        userId: newUser.id,
+      },
+    });
+
+    const { password: _, ...userWithoutPassword } = newUser;
+
+    return res.status(201).json({
+      message: "Kayıt başarılı.",
+      user: userWithoutPassword,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Sunucu hatası." });
+  }
+});
+
+router.post("/login", loginLimiter, async (req, res) => {
+  try {
+    const parsed = loginSchema.safeParse(req.body);
+    if (!parsed.success) {
+      const fieldErrors = parsed.error.issues.map((issue) => ({
+        field: issue.path[0],
+        message: issue.message,
+      }));
+      return res
+        .status(400)
+        .json({ message: "geçersiz veri ", errors: fieldErrors });
+    }
+    const { email, password } = parsed.data;
+
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+    if (!user) {
+      return res.status(401).json({ message: "E-posta veya şifre hatalı!" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "E-posta veya şifre hatalı." });
+    }
+
+    const token = jwt.sign(
+      { userId: user.id, username: user.username },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" },
+    );
+    const { password: _, ...userWithoutPassword } = user;
+    return res.status(200).json({
+      message: "Giriş başarılı.",
+      token,
+      user: userWithoutPassword,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Sunucu hatası." });
+  }
+});
+export default router;
